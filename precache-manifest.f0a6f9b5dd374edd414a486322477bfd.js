9818e6b8084958fdec277d5f355332dd94a5c877
Adesh Kumar <Adesh.kumar@bold.com>
2020-05-09 18:19:53 +0530

    Updates

---
+++ precache-manifest.f0a6f9b5dd374edd414a486322477bfd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7cba7d7d4371eae55580c1ce46ad67",
+    "url": "/soundable_app/index.html"
+  },
+  {
+    "revision": "e0dd3f572cbedc54c814",
+    "url": "/soundable_app/static/css/main.034bd96e.chunk.css"
+  },
+  {
+    "revision": "16f3173267698b38d686",
+    "url": "/soundable_app/static/js/2.971bc107.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/soundable_app/static/js/2.971bc107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dd3f572cbedc54c814",
+    "url": "/soundable_app/static/js/main.49753186.chunk.js"
+  },
+  {
+    "revision": "538d272e9f15fcdbbe06",
+    "url": "/soundable_app/static/js/runtime-main.8714222a.js"
+  },
+  {
+    "revision": "4663c752d0ae064a26941dbbc458f8d6",
+    "url": "/soundable_app/static/media/bubbles.4663c752.mp3"
+  },
+  {
+    "revision": "f989b24a16911445d9f09e4502aac2a3",
+    "url": "/soundable_app/static/media/clay.f989b24a.mp3"
+  },
+  {
+    "revision": "e56a0699465bd43c4484f76833070ef7",
+    "url": "/soundable_app/static/media/confetti.e56a0699.mp3"
+  },
+  {
+    "revision": "0bbef4baa8efcc4d69ef7d0cea3b73ca",
+    "url": "/soundable_app/static/media/glimmer.0bbef4ba.mp3"
+  },
+  {
+    "revision": "d8b9bdce5e2c23e2df40afd0f23b8832",
+    "url": "/soundable_app/static/media/green.d8b9bdce.mp3"
+  },
+  {
+    "revision": "83a83c31ef7392a0c624728b50e43190",
+    "url": "/soundable_app/static/media/moon.83a83c31.mp3"
+  },
+  {
+    "revision": "81715d016d1120bed5d9c1c8309b3b12",
+    "url": "/soundable_app/static/media/rock.81715d01.mp3"
+  },
+  {
+    "revision": "53ba10e6bf2625f43ea5fba4e1d290e3",
+    "url": "/soundable_app/static/media/ufo.53ba10e6.mp3"
+  }
+]);
